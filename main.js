9449c4dcc7e4b9306558cc084ec93e39e914c668
dawidgamedev <dawidgdev@op.pl>
2022-06-04 02:22:29 +0200

    js async

---
+++ main.js
@@ -0,0 +1,61 @@
+//Dark & Light Mode
+const toggleButton = document.querySelector('.switch-button');
+toggleButton.addEventListener('click', ()=>{
+    document.body.classList.toggle('lightmode');
+})
+
+//Api
+const searchInput = document.querySelector('.search-input');
+const searchButton = document.querySelector('.search-button');
+const username = document.querySelector('.username-data');
+const userid = document.querySelector('.id-data');
+const joindata = document.querySelector('.join-data');
+const bio = document.querySelector('.bio-data');
+const repos = document.querySelector('.repos-data-subtitle');
+const followers = document.querySelector('.followers-data-subtitle');
+const following = document.querySelector('.following-data-subtitle');
+
+const avatar = document.querySelector('.avatar').lastChild;
+let img = document.createElement("img");
+
+searchButton.addEventListener('click', ()=>{
+    const url = `https://api.github.com/users/${searchInput.value}`;
+
+    async function getUrl(){
+        let response = await fetch(url);
+        let data = await response.json();
+        
+        console.log(data);
+        gitSetup(data);
+    }
+    getUrl();
+})
+
+function gitSetup(data){
+    avatar.src = data.avatar_url;
+    if(data.name != null) {username.innerHTML = data.name}
+    else {username.innerHTML = data.login};
+    userid.innerHTML = `@${data.login}`;
+    joindata.innerHTML = `Joined ${data.created_at.slice(0, data.created_at.length = 10)}`;
+    if(data.bio != null) {bio.innerHTML = data.bio;}
+    else {bio.innerHTML = "This profile doesn't have bio";}
+    repos.innerHTML = data.public_repos;
+    followers.innerHTML = data.followers;
+    following.innerHTML = data.following;   
+}
+
+window.addEventListener('load', ()=>{
+    let myurl = `https://api.github.com/users/dawidwebdev`;
+
+    async function myUrl(){
+        let response = await fetch(myurl);
+        let data = await response.json();
+        
+        gitSetup(data);
+        console.log(data);
+    }
+    myUrl();
+})
+
+
+
