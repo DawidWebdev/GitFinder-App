0b0eb8576687ad82f90f556dda8ea0b21d2d9de9
dawidgamedev <dawidgdev@op.pl>
2022-06-07 00:33:56 +0200

    full project

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const username = document.querySelector('.username-data');
 const userid = document.querySelector('.id-data');
 const joindata = document.querySelector('.join-data');
 const bio = document.querySelector('.bio-data');
+const locationdata = document.querySelector('.location-text');
 const repos = document.querySelector('.repos-data-subtitle');
 const followers = document.querySelector('.followers-data-subtitle');
 const following = document.querySelector('.following-data-subtitle');
@@ -37,6 +38,8 @@ function gitSetup(data){
     else {username.innerHTML = data.login};
     userid.innerHTML = `@${data.login}`;
     joindata.innerHTML = `Joined ${data.created_at.slice(0, data.created_at.length = 10)}`;
+    if(data.location != null) {locationdata.innerHTML = data.location;}
+    else {locationdata.innerHTML = undefined}
     if(data.bio != null) {bio.innerHTML = data.bio;}
     else {bio.innerHTML = "This profile doesn't have bio";}
     repos.innerHTML = data.public_repos;
